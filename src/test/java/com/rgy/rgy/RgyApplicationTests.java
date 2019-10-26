56f21b32da2f6cd6e7195e3af0f5bf2b93ce695d
AnZhuJun <a17809283667@163.com>
2019-10-26 12:12:04 +0800

    init

---
+++ src/test/java/com/rgy/rgy/RgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgy.rgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
